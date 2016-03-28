7281306cc1f85b8dace753beccc4497e8519299c
Niklas Lindblad <niklas@lindblad.info>
2016-03-28 16:13:54 +0100

    Fix PEP8 violations

--- package-lambda.py
+++ package-lambda.py
@@ -17,49 +17,46 @@ File = namedtuple("File", ["absolutePath", "relativePath"])
 
 class Git(object):
 
-
     @staticmethod
     def executeInDirectory(directory, cmd):
         """Execute command in directory and return output"""
         current_working_directory = os.getcwd()
         os.chdir(directory)
-        proc = Popen(cmd, stdout = PIPE)
+        proc = Popen(cmd, stdout=PIPE)
         (stdout, stderr) = proc.communicate()
         os.chdir(current_working_directory)
         return (stdout, stderr)
 
-
     @staticmethod
     def getChecksum(directory):
         """Get Git commit SHA for current directory"""
-        stdout, stderr = Git.executeInDirectory(directory, ['git', 'show-ref'])
+        cmd = ['git', 'show-ref']
+        stdout, stderr = Git.executeInDirectory(directory, cmd)
         for row in stdout.split('\n'):
             if row.find('HEAD') != -1:
                 hash = row.split()[0]
                 break
         return hash
 
-
     @staticmethod
     def getUncommitedChanges(directory):
         """Return uncommitted changes"""
-        stdout, stderr = Git.executeInDirectory(directory, ['git', 'status', '-s'])
+        cmd = ['git', 'status', '-s']
+        stdout, stderr = Git.executeInDirectory(directory, cmd)
         return stdout.split('\n')
 
 
 class Packager(object):
 
-
     @staticmethod
-    def getDeduplicatedFileList(path, exclude = None):
+    def getDeduplicatedFileList(path, exclude=None):
         """Construct recursive list of files in path"""
         return list(Packager.deduplicateFileList(
             Packager.getFileList(path, exclude)
         ))
 
-
     @staticmethod
-    def getFileList(path, exclude = None):
+    def getFileList(path, exclude=None):
         """Construct recursive list of files in path"""
         logging.debug("Looking for files in directory %s", path)
         f = set()
@@ -73,9 +70,8 @@ class Packager(object):
                     f.add(file)
         return f
 
-
     @staticmethod
-    def shouldExclude(file, exclude = None):
+    def shouldExclude(file, exclude=None):
         """Determine if file should be excluded"""
         if not exclude:
             return False
@@ -83,18 +79,19 @@ class Packager(object):
             if file.relativePath.startswith(pattern):
                 return True
 
-
     @staticmethod
     def deduplicateFileList(file_list):
         """Deduplicate file list"""
         duplicated_files = set()
         for file in file_list:
             if file.relativePath.endswith(".pyc"):
-                uncompiled_file = File(file.absolutePath[:-1], file.relativePath[:-1])
+                uncompiled_file = File(
+                    file.absolutePath[:-1],
+                    file.relativePath[:-1]
+                )
                 if uncompiled_file in file_list:
                     duplicated_files.add(file)
         return file_list - duplicated_files
-
 
     @staticmethod
     def createZip(filename, file_list):
@@ -107,72 +104,72 @@ class Packager(object):
 def parse_arguments():
     """Parse command line arguments"""
     parser = argparse.ArgumentParser(
-        description = __doc__,
-        formatter_class = argparse.RawDescriptionHelpFormatter
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
     )
     parser.add_argument(
         "--strict",
-        help = "Disallow packaging if there are uncommitted Git changes",
-        action = "store_true"
+        help="Disallow packaging if there are uncommitted Git changes",
+        action="store_true"
     )
     parser.add_argument(
         "--debug",
-        help = "Increase output verbosity",
-        action = "store_true"
+        help="Increase output verbosity",
+        action="store_true"
     )
     parser.add_argument(
         "--source",
-        help = "Source directory of the Lambda code",
-        required = True
+        help="Source directory of the Lambda code",
+        required=True
     )
     parser.add_argument(
         "--libraries",
-        nargs = "*",
-        dest = "libraries",
-        required = False
+        nargs="*",
+        dest="libraries",
+        required=False
     )
     parser.add_argument(
         "--exclude",
-        nargs = "*",
-        dest = "exclude",
-        help = "Directories to exclude",
-        required = False
+        nargs="*",
+        dest="exclude",
+        help="Directories to exclude",
+        required=False
     )
     return parser
 
 
-def setup_logging(debug = False):
-  """Set up logging"""
-  root = logging.getLogger()
-  ch = logging.StreamHandler(sys.stdout)
-  formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-  ch.setFormatter(formatter)
-  root.addHandler(ch)
-  if debug:
-    ch.setLevel(logging.DEBUG)
-    root.setLevel(logging.DEBUG)
-  else:
-    ch.setLevel(logging.INFO)
-    root.setLevel(logging.INFO)
+def setup_logging(debug=False):
+    """Set up logging"""
+    root = logging.getLogger()
+    ch = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+    if debug:
+        ch.setLevel(logging.DEBUG)
+        root.setLevel(logging.DEBUG)
+    else:
+        ch.setLevel(logging.INFO)
+        root.setLevel(logging.INFO)
 
 
-def files_to_package(source_directory, library_directories = list(), exclude = list()):
+def files_to_package(sources, libraries=list(), exclude=list()):
     """Determine which files to package"""
-    source_directory = os.path.abspath(source_directory)
+    source_directory = os.path.abspath(sources)
     if not os.path.exists(source_directory):
         logging.error("No such source directory: %s", source_directory)
         sys.exit(1)
 
     source_files = Packager.getDeduplicatedFileList(
-        source_directory, exclude = exclude
+        source_directory, exclude=exclude
     )
 
     library_files = []
-    if library_directories:
-        library_directories = [os.path.abspath(d) for d in library_directories]
+    if libraries:
+        library_directories = [os.path.abspath(d) for d in libraries]
         for library_directory in library_directories:
             library_files += Packager.getDeduplicatedFileList(
-                library_directory, exclude = exclude
+                library_directory, exclude=exclude
             )
 
     return source_files + library_files
@@ -200,12 +197,12 @@ def enforce_strict(directory):
         sys.exit(2)
 
 
-def package(strict, source_directory, library_directories = list(), exclude = list()):
+def package(strict, sources, libraries=list(), exclude=list()):
     """Package Lambda from sources and libraries"""
     if strict:
-        enforce_strict(source_directory)
-    files = files_to_package(source_directory, library_directories, exclude)
-    output_filename = zip_filename(source_directory)
+        enforce_strict(sources)
+    files = files_to_package(sources, libraries, exclude)
+    output_filename = zip_filename(sources)
     Packager.createZip(output_filename, files)
     print("Created lambda zip file: %s" % output_filename)
 
